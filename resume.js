b5513356efd5684cb6a8aafeb2b0d9fedbaef7a2
Saranya6696 <107339275+Saranya6696@users.noreply.github.com>
2022-07-19 06:20:36 +0530

    Add files via upload

---
+++ resume.js
@@ -0,0 +1,58 @@
+var a = {
+    "name" : "SARANYA PRAMOTHKUMAR",
+    "designation" :  "AR CALLER",
+    "contact" : "Mobile : 875XXXXX97",
+    "Loc" : "Location : Chennai",
+    "Mailid" : "Mail : savi060696@gmail.com",
+}
+console.log(a["name"]);
+console.log(a["designation"]);
+console.log(a["contact"]);
+console.log(a["Loc"]);
+console.log(a["Mailid"]);
+
+var b = {
+    "Profile" : "Profile : Provide accurate product and service information to customer research available documentation including authorization nursing notes medical documentation on clients systems interpret explanation of benefits received etc prior to making the call",
+}
+
+console.log(b["Profile"]);
+
+var c = {
+    "Topic" : "Skills:",
+    "skill" : "Project Management", 
+    "skill1" : "Problem Solving", 
+    "skill2" : "Creativity",
+    "skill3" :  "Leardership",
+}
+     console.log(c["Topic"]);
+     console.log(c["skill"]);
+     console.log(c["skill1"]);
+     console.log(c["skill2"]);
+     console.log(c["skill3"]);
+
+
+var d = {
+    "Education" : "EDUCATION HISTORY",
+    "Graduation" : "B.COM IN COMMERCE : University of Madras - 90%",
+    "School" : "HIGH SCHOOL : St.Theresa girls higher secondary school - 93%", 
+}     
+console.log(d["Education"]);
+console.log(d["Graduation"]);
+console.log(d["School"]);
+
+var e = {
+    "Work" : "WORK EXPERIENCE",
+    "1ST" : "HCL TECHNOLOGIES - (Sep 2018 to Nov 2019)",
+    "ROLE" : "Customer Service Representative",
+    "2ND" : "ACCESS HEALTHCARE - (Nov 2019 to Mar 2021)",
+    "ROLE1" : "Client Partner",
+    "3RD" : "TECHNOSOFT GLOBAL SERVICES - (Mar 2021 till present )",
+    "ROLE2" : "Senoire Associate",
+}
+console.log(e["Work"]);
+console.log(e["1ST"]);
+console.log(e["ROLE"]);
+console.log(e["2ND"]);
+console.log(e["ROLE1"]);
+console.log(e["3RD"]);
+console.log(e["ROLE2"]);
